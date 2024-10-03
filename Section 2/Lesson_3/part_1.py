87c71625f85b863f0dbbf6db4136ce6ee5bb6e05
olegkatrichuk <katrichukoleg@gmail.com>
2024-10-03 12:35:21 +0300

    lesson 2_3

---
+++ Section 2/Lesson_3/part_1.py
@@ -0,0 +1,47 @@
+# task 1
+from sys import int_info
+
+# for i in range(1, 11):
+#     print(i * i)
+#
+# for i in range(1, 11):
+#     print(i ** 2)
+
+# task 2
+
+# for i in range(1, 31):
+#     if i % 3 == 0:
+#         print(i)
+
+# task 3
+
+# num = int(input('Enter a number : '))
+#
+# fact = 1
+#
+# for i in range(1, num + 1):
+#     fact *= i
+#
+# print(fact)
+
+# task 4
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f'{i} * {j} = {i * j}', end='\t')
+#     print()
+
+# task 5
+
+num = int(input('Enter a number : '))
+
+# if num > 0:
+#     temp1 = 0
+#     temp2 = 1
+#
+#     print(temp1, temp2, end=' ')
+#     for i in range(1, num):
+#         temp1, temp2 = temp2, temp1 + temp2
+#         print(temp2, end=' ')
+# else:
+#     print('Enter a positive number ')
